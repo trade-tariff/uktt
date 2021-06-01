f782ec81bbf7f056fb4fbba3cdb314dd28be9ce7
William Fish <william.michael.fish@gmail.com>
2021-06-01 10:20:38 +0100

    Removes unused module and constants

--- lib/uktt.rb
+++ lib/uktt.rb
@@ -1,12 +1,5 @@
 Dir[File.join(__dir__, 'uktt', 'resources', '*.rb')].sort.each { |file| require file }
 
-require 'uktt/version'
-require 'uktt/http'
-require 'uktt/parser'
-
-module Uktt
-  API_HOST_PROD           = 'https://www.trade-tariff.service.gov.uk'.freeze
-  API_HOST_LOCAL          = 'https://dev.trade-tariff.service.gov.uk'.freeze
-  API_VERSION             = 'v2'.freeze
-  DEFAULT_PARENT_CURRENCY = 'EUR'.freeze
-end
+require_relative 'uktt/version'
+require_relative 'uktt/http'
+require_relative 'uktt/parser'
